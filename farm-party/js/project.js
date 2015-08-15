dc16f3b82a4cdc58b415ca2275775346fc0932bc
ynabar <ynabar@gmail.com>
2015-08-15 09:05:16 -0400

    Farm Party

---
+++ farm-party/js/project.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$("#cow").animate
+	({right: "500"},5000);
+	({bottom: "700"},10000);
+
+	$("#lamb").animate
+	({left: "500"},5000);
+	({bottom: "700"},9000);
+
+	$("#horse").animate
+	({left: "2000"},10000);
+	({bottom: "900"},20000);
+
+	$("#rooster").animate
+	({right: "1500"},15000);
+	({top: "700"},10000);
+
+	$("#pig").animate
+	({left: "500"},30000);
+	({top: "700"},10000);
+
+});
